e0ccd23e70e5c6a0b4e1f6f21b464075faea4ce1
d-bohn <dna5021@psu.edu>
2017-09-24 19:06:53 -0400

    Fixed typos across pages, added a submission page, and rebuilt site.

--- R/make_site.R
+++ R/make_site.R
@@ -4,5 +4,9 @@
 # rmarkdown::render('deadly_sins.Rmd', output_format = 'html_document')
 # rmarkdown::render('RStudio-Markdown-Tips.Rmd', output_format = 'html_document')
 
-purrr::map(list.files(path='.', pattern = '.Rmd'), rmarkdown::render, output_format = 'html_document')
+files <- list.files(path='.', pattern = '.Rmd')
+
+# purrr::map(files, spelling::spell_check_files)
+
+purrr::map(files, rmarkdown::render, output_format = 'html_document')
 rmarkdown::render_site()
